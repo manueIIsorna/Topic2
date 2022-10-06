6f35974d2601ac2f73b8a7268b0ac019b36dff78
misorna <misorna@106-02.info2>
2022-10-06 13:55:58 +0200

    commit

--- Boletin1/src/exercises/Ejercicio1.java
+++ Boletin1/src/exercises/Ejercicio1.java
@@ -1,7 +1,35 @@
 package exercises;
 
+import java.util.Scanner;
+
+/***
+ * Diseñar una aplicación que solicite al usuario un número e indique si es par
+ * o impar.
+ * 
+ * @author misorna
+ */
 public class Ejercicio1 {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+		// Bloque de variables
+		int num;
+		int resto;
+		Scanner sc = new Scanner(System.in);
+
+		// Pedimos al usuario el numero
+		System.out.print("Introduzca un numero entero: ");
+		num = sc.nextInt(); // leemos de teclado
+
+		// Operamos para sacar el resto
+		resto = num % 2;
+
+		// Vemos si el numero es par o no
+		if (resto == 0) { // Si sale cero sería par
+			System.out.println("El número es par");
+		} else { // Si sale un número distinto de cero sería impar
+			System.out.println("El número es impar");
+		}
+
+		// Cerramos el Scanner
+		sc.close();
+	}
 }
